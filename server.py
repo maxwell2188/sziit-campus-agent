6772acdb3c1cf398de65ebc48376e564c62ffab7
Maxwell <527081591@qq.com>
2026-07-27 15:10:49 +0800

    fix: 生产环境关闭reload模式

--- server.py
+++ server.py
@@ -762,4 +762,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
